240182067258de9fa8ca829ef38585754c8f2c1f
anurag jaiswal <anuraggjaiswal@gmail.com>
2024-10-03 22:08:36 +0530

    Resolved eslint issue

--- constants/index.js
+++ constants/index.js
@@ -1,88 +1,88 @@
 export const exploreWorlds = [
   {
-    id: 'world-1',
-    imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "The Hogwarts",
   },
   {
-    id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "The Upside Down",
   },
   {
-    id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Uranaus Plate',
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Uranaus Plate",
   },
   {
-    id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "Paradise Island",
   },
   {
-    id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "Hawkins Labs",
   },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+  "Find a world that suits you and you want to enter",
+  "Enter the world by reading basmalah to be safe",
+  "No need to beat around the bush, just stay on the gas and have fun",
 ];
 
 export const newFeatures = [
   {
-    imgUrl: '/vrpano.svg',
-    title: 'A new world',
+    imgUrl: "/vrpano.svg",
+    title: "A new world",
     subtitle:
-        'we have the latest update with new world for you to try never mind',
+      "we have the latest update with new world for you to try never mind",
   },
   {
-    imgUrl: '/headset.svg',
-    title: 'More realistic',
+    imgUrl: "/headset.svg",
+    title: "More realistic",
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+      "In the latest update, your eyes are narrow, making the world more realistic than ever",
   },
 ];
 
 export const insights = [
   {
-    imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk nervous',
+    imgUrl: "/planet-06.png",
+    title: "The launch of the Metaverse makes Elon musk nervous",
     subtitle:
-        'Significant time is spent on managing the element of discomfort. There’s even a suggestion of restless worry. ',
+      "Significant time is spent on managing the element of discomfort. There’s even a suggestion of restless worry. ",
   },
   {
-    imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
+    imgUrl: "/planet-07.png",
+    title: "7 tips to easily master the madness of the Metaverse",
     subtitle:
-        'Life is guided by connection and consequence, done with ease and grace. Great challenges, though seemingly insurmountable, often give birth to trivial outcomes.',
+      "Life is guided by connection and consequence, done with ease and grace. Great challenges, though seemingly insurmountable, often give birth to trivial outcomes.",
   },
   {
-    imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
+    imgUrl: "/planet-08.png",
+    title: "With one platform you can explore the whole world virtually",
     subtitle:
-        'Each person shapes their own path, no matter how challenging the elements may be. Living freely means balancing learning and growth, with the right rhythm.',
+      "Each person shapes their own path, no matter how challenging the elements may be. Living freely means balancing learning and growth, with the right rhythm.",
   },
 ];
 
 export const socials = [
   {
-    name: 'twitter',
-    url: '/twitter.svg',
+    name: "twitter",
+    url: "/twitter.svg",
   },
   {
-    name: 'linkedin',
-    url: '/linkedin.svg',
+    name: "linkedin",
+    url: "/linkedin.svg",
   },
   {
-    name: 'instagram',
-    url: '/instagram.svg',
+    name: "instagram",
+    url: "/instagram.svg",
   },
   {
-    name: 'facebook',
-    url: '/facebook.svg',
+    name: "facebook",
+    url: "/facebook.svg",
   },
 ];
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,31 +1,48 @@
-'use client';
+"use client";
 
-import {motion} from "framer-motion"
-import styles from "../styles"
-import{fadeIn, staggerContainer} from "../utils/motion"
-import { ExploreCard,TitleText,TypingText } from "../components";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
 import { useState } from "react";
-import {eploreWorlds, exploreWorlds} from "../constants"
+import { eploreWorlds, exploreWorlds } from "../constants";
 import World from "./World";
 
 const Explore = () => {
-  const [active, setActive] = useState('world-2')
+  const [active, setActive] = useState("world-2");
   return (
     <section className={`${styles.paddings} `} id="explore">
-    <motion.div variants={staggerContainer} initial="hidden" whileInView="show" viewport={{once:false ,amount:0.25}}
-    className={`${styles.innerWidth} mx-auto flex flex-col`}>
-      <TypingText title="| The World" textStyles="text-center"/>
-      <TitleText title={<>Choose the World you want <br className="md:block hidden"/> to explore</>} textStyles="text-center"/>
-      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5  ">
-        {exploreWorlds.map((world,index)=>(
-            <ExploreCard key={World.id} {...world} index={index} active={active} handleClick={setActive} />
-
-        ))}
-      </div>
-    </motion.div>
-  </section>
-  )
-  
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the World you want <br className="md:block hidden" /> to
+              explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5  ">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={World.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Explore;
